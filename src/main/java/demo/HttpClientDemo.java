c74bfbb8ee2365bf6b80c5baeb3a79d8fe3a2ce3
hhgzxn <you@example.com>
2020-06-22 01:22:21 +0800

    first submition

---
+++ src/main/java/demo/HttpClientDemo.java
@@ -0,0 +1,173 @@
+package demo;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sun.deploy.net.URLEncoder;
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: HttpClientDemo
+ * @Description:
+ * @author: Tony.Cheung
+ * @create: 2020/6/22 1:07
+ **/
+public class HttpClientDemo {
+
+    /**
+     * @param
+     * @return
+     * @Title: GET
+     * @Description:
+     * @Date: 2020/6/21 23:48
+     */
+    public static void doGet(String url, Map<String, String> paramMaps) {
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        // params
+        StringBuffer params = new StringBuffer();
+        paramMaps.forEach((k, v) -> {
+            params.append(k + "=" + v + "&");
+        });
+
+        // Create Get Request
+        HttpGet httpGet = new HttpGet(url + "?" + params);
+        // Response to Model
+        CloseableHttpResponse response = null;
+        try {
+            // config info
+            RequestConfig requestConfig = RequestConfig.custom()
+                    // 设置连接超时时间(单位毫秒)
+                    .setConnectTimeout(5000)
+                    // 设置请求超时时间(单位毫秒)
+                    .setConnectionRequestTimeout(5000)
+                    // socket读写超时时间(单位毫秒)
+                    .setSocketTimeout(5000)
+                    // 设置是否允许重定向(默认为true)
+                    .setRedirectsEnabled(true).build();
+
+            // 将上面的配置信息 运用到这个Get请求里
+            httpGet.setConfig(requestConfig);
+
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param
+     * @return
+     * @Title: POST
+     * @Description:
+     * @Date: 2020/6/22 1:07
+     */
+    public void doPostTestTwo() {
+
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        // 创建Post请求
+        HttpPost httpPost = new HttpPost("http://localhost:8080/testPost");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "潘晓婷");
+        jsonObject.put("age", 18);
+        jsonObject.put("gender", "女");
+        jsonObject.put("motto", "姿势要帅");
+        Map<String, Object> param = new HashMap<>();
+        param.put("user", jsonObject);
+
+        // 我这里利用阿里的fastjson，将Object转换为json字符串;
+        // (需要导入com.alibaba.fastjson.JSON包)
+        String jsonString = JSONObject.toJSONString(param);
+
+        StringEntity entity = new StringEntity(jsonString, "UTF-8");
+
+        // post请求是将参数放在请求体里面传过去的;这里将entity放入post请求体中
+        httpPost.setEntity(entity);
+
+        httpPost.setHeader("Content-Type", "application/json;charset=utf8");
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Post请求
+            response = httpClient.execute(httpPost);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String url = "http://localhost:8080/testGet";
+        HttpClientDemo.doGet(url, new HashMap<String, String>() {{
+            put("name", "tony");
+            put("age", "24");
+        }});
+        //httpClientDemo.doPostTestTwo();
+    }
+}
